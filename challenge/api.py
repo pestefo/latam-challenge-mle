6c87ea7a70b23b27f9114367be576096c1f40fff
Pablo Estefó <pestefo@gmail.com>
2023-12-18 17:04:11 -0300

    chore(*): black pass

--- challenge/api.py
+++ challenge/api.py
@@ -2,11 +2,11 @@ import fastapi
 
 app = fastapi.FastAPI()
 
+
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
-    return {
-        "status": "OK"
-    }
+    return {"status": "OK"}
+
 
 @app.post("/predict", status_code=200)
 async def post_predict() -> dict:
