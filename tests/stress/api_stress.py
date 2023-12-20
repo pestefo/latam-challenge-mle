59259cb02065cd710a4797448d413ade06b71a5c
Pablo Estefó <pestefo@gmail.com>
2023-12-19 21:20:45 -0300

    chore(api_stress.py): black pass

--- tests/stress/api_stress.py
+++ tests/stress/api_stress.py
@@ -1,34 +1,29 @@
 from locust import HttpUser, task
 
+
 class StressUser(HttpUser):
-    
     @task
     def predict_argentinas(self):
         self.client.post(
-            "/predict", 
+            "/predict",
             json={
                 "flights": [
                     {
-                        "OPERA": "Aerolineas Argentinas", 
-                        "TIPOVUELO": "N", 
-                        "MES": 3
+                        "OPERA": "Aerolineas Argentinas",
+                        "TIPOVUELO": "N",
+                        "MES": 3,
                     }
                 ]
-            }
+            },
         )
-
 
     @task
     def predict_latam(self):
         self.client.post(
-            "/predict", 
+            "/predict",
             json={
                 "flights": [
-                    {
-                        "OPERA": "Grupo LATAM", 
-                        "TIPOVUELO": "N", 
-                        "MES": 3
-                    }
+                    {"OPERA": "Grupo LATAM", "TIPOVUELO": "N", "MES": 3}
                 ]
-            }
+            },
         )
